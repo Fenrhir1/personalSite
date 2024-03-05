764c67a78ae198a3973597bf2a6e70c44771c0d3
Salvatore Polizzotti <fenrhir@libero.it>
2024-03-05 10:04:10 +0100

    first push of base app

---
+++ src/App.tsx
@@ -0,0 +1,91 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="section-banner">
+        <div id="star-1">
+          <div className="curved-corner-star">
+            <div id="curved-corner-bottomright"></div>
+            <div id="curved-corner-bottomleft"></div>
+          </div>
+          <div className="curved-corner-star">
+            <div id="curved-corner-topright"></div>
+            <div id="curved-corner-topleft"></div>
+          </div>
+        </div>
+
+        <div id="star-2">
+          <div className="curved-corner-star">
+            <div id="curved-corner-bottomright"></div>
+            <div id="curved-corner-bottomleft"></div>
+          </div>
+          <div className="curved-corner-star">
+            <div id="curved-corner-topright"></div>
+            <div id="curved-corner-topleft"></div>
+          </div>
+        </div>
+
+        <div id="star-3">
+          <div className="curved-corner-star">
+            <div id="curved-corner-bottomright"></div>
+            <div id="curved-corner-bottomleft"></div>
+          </div>
+          <div className="curved-corner-star">
+            <div id="curved-corner-topright"></div>
+            <div id="curved-corner-topleft"></div>
+          </div>
+        </div>
+
+        <div id="star-4">
+          <div className="curved-corner-star">
+            <div id="curved-corner-bottomright"></div>
+            <div id="curved-corner-bottomleft"></div>
+          </div>
+          <div className="curved-corner-star">
+            <div id="curved-corner-topright"></div>
+            <div id="curved-corner-topleft"></div>
+          </div>
+        </div>
+
+        <div id="star-5">
+          <div className="curved-corner-star">
+            <div id="curved-corner-bottomright"></div>
+            <div id="curved-corner-bottomleft"></div>
+          </div>
+          <div className="curved-corner-star">
+            <div id="curved-corner-topright"></div>
+            <div id="curved-corner-topleft"></div>
+          </div>
+        </div>
+
+        <div id="star-6">
+          <div className="curved-corner-star">
+            <div id="curved-corner-bottomright"></div>
+            <div id="curved-corner-bottomleft"></div>
+          </div>
+          <div className="curved-corner-star">
+            <div id="curved-corner-topright"></div>
+            <div id="curved-corner-topleft"></div>
+          </div>
+        </div>
+
+        <div id="star-7">
+          <div className="curved-corner-star">
+            <div id="curved-corner-bottomright"></div>
+            <div id="curved-corner-bottomleft"></div>
+          </div>
+          <div className="curved-corner-star">
+            <div id="curved-corner-topright"></div>
+            <div id="curved-corner-topleft"></div>
+          </div>
+        </div>
+      </div>
+      <h1>Salvatore Polizzotti</h1>
+
+      <p className="read-the-docs">Coming soon...</p>
+    </>
+  );
+}
+
+export default App;

0e11b12a0a150d59379bb93ee9f34068151b8577
Salvatore Polizzotti <salvatore.polizzotti@libero.it>
2024-03-13 18:41:52 +0100

    fixed minor errors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { Avatar, Drawer } from "@mui/material";
+import { Avatar } from "@mui/material";
 import TemporaryDrawer from "./Drawer";
 
 export default function Navbar() {
